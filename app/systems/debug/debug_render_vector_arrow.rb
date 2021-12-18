a402265fc290cbadf51b3d72925657f1f5cb84e4
arngo <27396817+arngo@users.noreply.github.com>
2021-12-17 23:40:59 -0500

    center sprite on boid position

--- app/systems/debug/debug_render_vector_arrow.rb
+++ app/systems/debug/debug_render_vector_arrow.rb
@@ -1,21 +1,19 @@
 FF::Sys.new('DebugRenderVectorArrow', priority: 100) do
   FF::Cmp::DebugVectorArrow[0].entities.each do |entity|
     boid = entity.components[FF::Cmp::Boid][0]
-    ox = boid.x + entity.components[FF::Cmp::Sprite][0].props.w / 2
-    oy = boid.y + entity.components[FF::Cmp::Sprite][0].props.h / 2
     length = FF::Cmp::DebugVectorArrow[0].length
     #puts "vx: #{boid.vx}"
     #puts "cx: #{boid.cx}"
-    vxtip = ox + (boid.vx * length)
-    vytip = oy + (boid.vy * length)
+    vxtip = boid.x + (boid.vx * length)
+    vytip = boid.y + (boid.vy * length)
 
     # Velocity
-    $gtk.args.outputs.lines << [ox, oy, vxtip, vytip, 0, 255, 255, 255]
-    $gtk.args.outputs.lines << [ox+1, oy, vxtip+1, vytip, 0, 255, 255, 255]
-    $gtk.args.outputs.lines << [ox-1, oy, vxtip-1, vytip, 0, 255, 255, 255]
-    $gtk.args.outputs.lines << [ox, oy+1, vxtip, vytip+1, 0, 255, 255, 255]
-    $gtk.args.outputs.lines << [ox, oy-1, vxtip, vytip-1, 0, 255, 255, 255]
-    #$gtk.args.outputs.lines << [ox, oy, ox + (boid.cx * length * 10), oy + (boid.cy * length), 0, 255, 0, 255]
+    $gtk.args.outputs.lines << [boid.x, boid.y, vxtip, vytip, 0, 255, 255, 255]
+    $gtk.args.outputs.lines << [boid.x+1, boid.y, vxtip+1, vytip, 0, 255, 255, 255]
+    $gtk.args.outputs.lines << [boid.x-1, boid.y, vxtip-1, vytip, 0, 255, 255, 255]
+    $gtk.args.outputs.lines << [boid.x, boid.y+1, vxtip, vytip+1, 0, 255, 255, 255]
+    $gtk.args.outputs.lines << [boid.x, boid.y-1, vxtip, vytip-1, 0, 255, 255, 255]
+    #$gtk.args.outputs.lines << [boid.x, boid.y, boid.x + (boid.cx * length * 10), boid.y + (boid.cy * length), 0, 255, 0, 255]
 
     # Change in Velocity
     $gtk.args.outputs.lines << [vxtip, vytip, vxtip + (boid.cx * length * 10), vytip + (boid.cy * length * 10), 255, 0, 255, 255]
--- app/systems/update_boid_sprite.rb
+++ app/systems/update_boid_sprite.rb
@@ -1,8 +1,9 @@
 FF::Scn::Render.add(
   FF::Sys.new('UpdateBoidSprite', priority: 98) do
     FF::Cmp::Boid.each do |boid|
-      boid.entities[0].components[FF::Cmp::Sprite][0].props[:x] = boid.x
-      boid.entities[0].components[FF::Cmp::Sprite][0].props[:y] = boid.y
+      sprite = boid.entities[0].components[FF::Cmp::Sprite][0]
+      sprite.props[:x] = boid.x - sprite.props[:w] / 2
+      sprite.props[:y] = boid.y - sprite.props[:h] / 2
     end
   end
 )
