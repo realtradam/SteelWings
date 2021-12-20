26cc939ac782a5c8db3afc41bce0f5316f386b7c
realtradam <github@tradam.dev>
2021-12-19 23:15:27 -0500

    .

--- app/factories/ships/osprey.rb
+++ app/factories/ships/osprey.rb
@@ -2,9 +2,18 @@ class Factory
   class Osprey
     def self.new(x: x, y: y)
       sprite = FF::Cmp::Sprite.new
-      sprite.props[:path] = 'sprites/kenny/Ships/Osprey.png'
+      sprite.props[:path] = [
+        'sprites/kenny/Ships/WeebillGrey.png',
+        'sprites/kenny/Ships/BuntingGrey.png',
+        'sprites/kenny/Ships/NighthawkGrey.png',
+        'sprites/kenny/Ships/MagpieGrey.png',
+        'sprites/kenny/Ships/WaxwingGrey.png',
+        'sprites/kenny/Ships/LongspurGrey.png',
+        'sprites/kenny/Ships/WarblerGrey.png',
+        'sprites/kenny/Ships/NutcrackerGrey.png',
+      ].sample
       FF::Ent.new(
-        FF::Cmp::Boid.new(x: x, y: y, vx: 0, vy: 0, w: 32, h: 32),
+        FF::Cmp::Boid.new(x: x, y: y, vx: -3, vy: -3, w: 32, h: 32),
         sprite,
         FF::Cmp::BoidBounds.new(strength: 0.6),
         FF::Cmp::BoidsAlignment.new(strength: 1),
@@ -13,6 +22,7 @@ class Factory
         FF::Cmp::Hp.new(health: 100),
         FF::Cmp::CollisionDamage.new(damage: 100),
         FF::Cmp::Hitcircle.new(r: 12),
+        FF::Cmp::BoidMinimumSpeed.new(speed: 3),
         FF::Cmp::Team.new,
         FF::Cmp::SingletonDebugVectorArrow[0],
         FF::Cmp::SingletonCamera[0],
