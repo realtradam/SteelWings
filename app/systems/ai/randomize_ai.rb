3ca36a2bf0cdba5d75d453ecd2be3b265d058460
arngo <27396817+arngo@users.noreply.github.com>
2021-12-26 17:28:38 -0500

    clone entities to properly remove all ai pick components

--- app/systems/ai/randomize_ai.rb
+++ app/systems/ai/randomize_ai.rb
@@ -4,30 +4,30 @@ FF::Scn::BoidRules.add(
     timer.timer += 1
     if timer.timer > timer.interval
       random_ai_pick = FF::Cmp::SingletonRandomAIPick[0]
-      random_ai_pick.entities.each do |entity|
+      random_ai_pick.entities.clone.each do |entity|
         entity.remove random_ai_pick
-        puts 'remove pick'
+        #puts 'remove pick'
       end
       FF::Cmp::BoidsSeparation.each do |sep|
         if rand < 0.3
           sep.entities[0].add random_ai_pick
-          puts 'pick'
+          #puts 'pick'
         end
       end
       x = rand 3
       if x == 0
         FF::Sys::Scatter.call
-        puts 'scatter'
+        #puts 'scatter'
       elsif x == 1
         FF::Sys::Rejoin.call
-        puts 'rejoin'
+        #puts 'rejoin'
       elsif x == 2
         FF::Sys::TargetPlayer.call
-        puts 'target'
+        #puts 'target'
       end
       #timer.interval = (300..1200).to_a.sample
       timer.interval = 60
-      puts timer.interval
+      #puts timer.interval
       timer.timer = 0
     end
   end
